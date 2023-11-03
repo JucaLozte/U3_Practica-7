1e8e5ab3714ea6453daca515110edc29a54f0cb8
JucaLozte <carloste653@gmail.com>
2023-11-03 18:03:24 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace U3_Practica_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(22, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(489, 26);
+            label1.TabIndex = 0;
+            label1.Text = "Imprime números de un rango y calcula la suma";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(25, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(127, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Primer número";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(27, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(125, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Último número";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(158, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 28);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(158, 142);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 28);
+            textBox2.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.Salmon;
+            listBox1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(324, 80);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 256);
+            listBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(99, 213);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Imprimir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(158, 299);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 28);
+            textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(45, 302);
+            label4.Name = "label4";
+            label4.Size = new Size(107, 21);
+            label4.TabIndex = 8;
+            label4.Text = "La suma es:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSalmon;
+            ClientSize = new Size(523, 373);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Suma números en rango";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button1;
+        private TextBox textBox3;
+        private Label label4;
+    }
+}
